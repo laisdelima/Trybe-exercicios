b3c0c02352f3a5c9e3a09b24b009495c189be5c6
Laís de Lima <laispsico92@gmail.com>
2023-01-29 17:32:53 -0300

    atividade 1 do dia 3.1 feita

---
+++ 01-Fundamentos/secao-03-introducao-JS/dia-01-primeiros-passos/atividade1.js
@@ -0,0 +1,25 @@
+/* Crie uma constante chamada myName e atribua a ela o seu nome.
+Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+Crie uma variável let chamada birthYear e atribua a ela o ano em que você nasceu.
+Utilize o console.log() para imprimir as constantes e variáveis que você criou. */
+
+const myName = 'Laís';
+const birthCity = 'Quirinópolis';
+let birthYear = 1992;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+/* Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece! */
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+/* Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔 */
+
+birthCity = 'Anápolis';
+
+console.log(birthCity);
+/* O código mostra uma mensagem de erro porque não é possível reatribuir um valor à uma constante! */
