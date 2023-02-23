e93ae3436fbd5ab77302ba57a9424e40fa73fedd
Laís de Lima <laispsico92@gmail.com>
2023-02-23 16:19:07 -0300

    adicionando exercício sobre lucro de vendas

--- 01-Fundamentos/secao-03-introducao-JS/dia-01-primeiros-passos/exercicios_finais.js
+++ 01-Fundamentos/secao-03-introducao-JS/dia-01-primeiros-passos/exercicios_finais.js
@@ -140,3 +140,22 @@ if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
 } else {
     console.log('false');
 }
+
+// Utilize if/else para escrever um código que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda. A partir dos valores, o código deve calcular o lucro (ou seja, o valor de venda menos o custo do produto) da empresa ao vender mil produtos.
+// Atente para o fato de que um imposto de 20% incide sobre o custo do produto.
+
+// Seu código deve emitir uma mensagem de erro e encerrar caso algum dos valores de entrada seja menor que zero.
+
+// O lucro de um produto é o resultado da subtração do valor de venda pelo custo e deve considerar que o imposto de 20% faz parte do valor de custo.
+
+let valorCusto = 245;
+let valorVenda = 400;
+
+// imposto de 20% = 1 + 0,2 = 1,2.
+if (valorCusto < 0 || valorVenda < 0) {
+    console.log('Erro, valor incompatível!');
+} else {
+    let valorCustoTotal = valorCusto * 1.2;
+    let lucroTotalEmpresa = (valorVenda - valorCustoTotal) * 1000;
+    console.log(lucroTotalEmpresa);
+}
