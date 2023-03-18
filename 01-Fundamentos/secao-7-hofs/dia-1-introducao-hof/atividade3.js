a5938f0f0e461e0d8ce2dc935d5c79a0b88b4094
Laís de Lima <laispsico92@gmail.com>
2023-03-18 15:13:03 -0300

    atividade 3 do dia 7.1 finalizada

--- 01-Fundamentos/secao-7-hofs/dia-1-introducao-hof/atividade3.js
+++ 01-Fundamentos/secao-7-hofs/dia-1-introducao-hof/atividade3.js
@@ -11,9 +11,26 @@
 
 const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
-const hasName = (arr, name) => {
-  //Adicione seu código aqui
-};
+const hasName = (arr, name) => arr.some((actualName) => actualName === name);
 
-console.log(hasName(names, 'Ana'));
-console.log(hasName(names, 'Pedro'));
+// console.log(hasName(names, 'Ana'));
+// console.log(hasName(names, 'Pedro'));
+
+// Você ficou responsável por criar um sistema que verifica se as pessoas da mesma turma possuem uma determinada faixa de idade. Você deve utilizar o array people e desenvolver a sua lógica a partir dele. Para isso:
+
+// Crie uma função que verifica se todas as pessoas do array people possuem a idade mínima especificada;
+
+// Retorne true se todas tiverem a idade maior ou igual à mínima e, caso contrário, false.
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+  const verifyAges = (arr, minimumAge) => arr.every((person) => person.age >= minimumAge);
+
+console.log(verifyAges(people, 18));
+console.log(verifyAges(people, 14));
