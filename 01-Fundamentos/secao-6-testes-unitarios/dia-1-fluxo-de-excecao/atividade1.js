9a8bcd6250f088639611c74d3af9450e2a732bbf
Laís de Lima <laispsico92@gmail.com>
2023-03-22 17:20:05 -0300

    finalizando exercícios dia 6.1

--- 01-Fundamentos/secao-6-testes-unitarios/dia-1-fluxo-de-excecao/atividade1.js
+++ 01-Fundamentos/secao-6-testes-unitarios/dia-1-fluxo-de-excecao/atividade1.js
@@ -65,4 +65,34 @@ const order = {
   };
   
   orderModifier(order);
-  
+
+// Parte III - Trablahando em uma autoescola.
+// A função studentRegister() recebe dois parâmetros: name, nome da pessoa que quer começar a fazer as aulas; e age, idade da pessoa no momento do cadastro.
+// Caso a função não receba name ou idade (ou ambos), lance uma exceção com a mensagem 'Todas as informações são necessárias'.
+// Caso a função receba o nome e uma idade inferior a 18 anos, lance uma exceção com a mensagem 'Ops, infelizmente nesse momento você não pode fazer as aulas'
+// Caso a função receba o nome e uma idade igual ou superior a 18 anos, retorne a string: 'Nome, seja bem-vindo(a) à AuTrybe!'
+
+const validateData = (name, age) => {
+  if (!name || !age) {
+    throw new Error('Todas as informações são necessárias');
+  }
+};
+
+const validateMinorAge = (age) => {
+  if (age < 18) {
+    throw new Error('Ops, infelizmente nesse momento você não pode fazer as aulas');
+  }
+};
+
+const studentRegister = (name, age) => {
+  try {
+    validateData(name, age);
+    validateMinorAge(age);
+    return `${name}, seja bem-vindo(a) à AuTrybe!`;
+  } catch (error) {
+    return error.message;
+  }
+};
+console.log(studentRegister('Laís', 30));
+console.log(studentRegister('Laís', 15));
+console.log(studentRegister('Laís'));
