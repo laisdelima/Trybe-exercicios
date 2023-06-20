2697ee04cd5004c7b0de7a8364ec447ac0c11495
Laís de Lima <laispsico92@gmail.com>
2023-06-20 18:54:53 -0300

    finalizando exemplo contador

--- 02-Front-end/secao-7-redux/dia-2-react-redux/exemplo1/my-app/src/App.js
+++ 02-Front-end/secao-7-redux/dia-2-react-redux/exemplo1/my-app/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import './App.css';
-import { actionCreator } from './redux/actions'; 
+import { actionCreator, clickCounter } from './redux/actions'; 
 
 class App extends React.Component {
   render() {
-    const { countState, dispatch } = this.props;
+    const { countState, dispatch, clickCount } = this.props;
+    const dispatchAll = (add = 1) => {
+      dispatch(actionCreator(add));
+      dispatch(clickCounter());
+    };
+
     return (
       <div className="App">
         <h1>Contador</h1>
         <h2>{ countState }</h2>
         <button
-          onClick={() => dispatch(actionCreator(1))}
+          onClick={() => dispatchAll()}
         >
           Incrementar 1
         </button>
 
         <button
-          onClick={() => dispatch(actionCreator(5))}
+          onClick={() => dispatchAll(5)}
         >
           Incrementar 5
         </button>
+        <h3>Número de clicks: { clickCount }</h3>
       </div>
     );
   }
@@ -28,6 +34,7 @@ class App extends React.Component {
 
 const mapStateToProps = (state) => ({
   countState: state.count,
+  clickCount: state.clicks,
 });
 
 export default connect(mapStateToProps)(App);
--- 02-Front-end/secao-7-redux/dia-2-react-redux/exemplo1/my-app/src/redux/reducers/counterReducer.js
+++ 02-Front-end/secao-7-redux/dia-2-react-redux/exemplo1/my-app/src/redux/reducers/counterReducer.js
@@ -1,11 +1,21 @@
-const INITIAL_STATE = { count: 0 };
+const INITIAL_STATE = {
+  count: 0,
+  clicks: 0,
+};
 
 const counterReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'INCREMENT_COUNTER':
-      return { 
+      return {
+        ...state,
         count: state.count + action.payload,
       };
+
+      case 'INCREMENT_CLICK':
+        return {
+          ...state,
+          clicks: state.clicks + 1,
+        }
 
       default:
         return state;
