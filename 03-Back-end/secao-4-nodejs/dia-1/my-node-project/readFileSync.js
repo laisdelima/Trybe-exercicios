f52e8dd7cd2193babafdc73ab447af10857ec362
Laís de Lima <laispsico92@gmail.com>
2023-08-30 17:42:57 -0300

    finaliza exercicio

---
+++ 03-Back-end/secao-4-nodejs/dia-1/my-node-project/readFileSync.js
@@ -0,0 +1,26 @@
+// método síncrono
+// const fs = require('fs');
+
+// const nomeDoArquivo = 'meu-arquivo.txt';
+
+// try {
+//   const data = fs.readFileSync(nomeDoArquivo, 'utf8');
+//   console.log(data);
+// } catch (err) {
+//   console.error(`Erro ao ler o arquivo: ${err.path}`);
+//   console.log(err);
+// }
+
+// método async
+const fs = require('fs').promises;
+
+async function main() {
+  try {
+    const data = await fs.readFile('./meu-arquivo.txt', 'utf-8');
+    console.log(data);
+  } catch (err) {
+    console.error(`Erro ao ler o arquivo: ${err.message}`);
+  }
+}
+
+main()
