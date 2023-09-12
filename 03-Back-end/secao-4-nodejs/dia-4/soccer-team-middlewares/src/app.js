9bfd13084ea6d06a4d86ed07923028dac6ffadc5
Laís de Lima <laispsico92@gmail.com>
2023-09-12 10:54:23 -0300

    parte 1 exemplo de middleware assíncrono

--- 03-Back-end/secao-4-nodejs/dia-4/soccer-team-middlewares/src/app.js
+++ 03-Back-end/secao-4-nodejs/dia-4/soccer-team-middlewares/src/app.js
@@ -4,10 +4,12 @@ const existingId = require('./middlewares/existingId');
 const teams = require('./utils/teams');
 
 const app = express();
+const apiCredentials = require('./middlewares/apiCredentials');
 
 let nextId = 3;
 
 app.use(express.json());
+app.use(apiCredentials);
 
 app.get('/teams', (req, res) => res.json(teams));
 
@@ -39,15 +41,15 @@ app.get('/teams', (req, res) => res.json(teams));
 //   }
 // });
 
-app.get('/teams/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find((t) => t.id === id);
-  if (team) {
-    res.json(team);
-  } else {
-    res.sendStatus(404);
-  }
-});
+// app.get('/teams/:id', (req, res) => {
+//   const id = Number(req.params.id);
+//   const team = teams.find((t) => t.id === id);
+//   if (team) {
+//     res.json(team);
+//   } else {
+//     res.sendStatus(404);
+//   }
+// });
 
 // Arranja os middlewares para chamar validateTeam primeiro
 app.post('/teams', validateTeam, (req, res) => {
@@ -57,28 +59,28 @@ app.post('/teams', validateTeam, (req, res) => {
   res.status(201).json(team);
 });
 
-app.put('/teams/:id', validateTeam, (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find((t) => t.id === id);
-  if (team) {
-    const index = teams.indexOf(team);
-    const updated = { id, ...req.body };
-    teams.splice(index, 1, updated);
-    res.status(201).json(updated);
-  } else {
-    res.sendStatus(400);
-  }
-});
+// app.put('/teams/:id', validateTeam, (req, res) => {
+//   const id = Number(req.params.id);
+//   const team = teams.find((t) => t.id === id);
+//   if (team) {
+//     const index = teams.indexOf(team);
+//     const updated = { id, ...req.body };
+//     teams.splice(index, 1, updated);
+//     res.status(201).json(updated);
+//   } else {
+//     res.sendStatus(400);
+//   }
+// });
 
-app.delete('/teams/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find((t) => t.id === id);
-  if (team) {
-    const index = teams.indexOf(team);
-    teams.splice(index, 1);
-  }
-  res.sendStatus(204);
-});
+// app.delete('/teams/:id', (req, res) => {
+//   const id = Number(req.params.id);
+//   const team = teams.find((t) => t.id === id);
+//   if (team) {
+//     const index = teams.indexOf(team);
+//     teams.splice(index, 1);
+//   }
+//   res.sendStatus(204);
+// });
 
 // --------------------------------------------
 // Para fixar
