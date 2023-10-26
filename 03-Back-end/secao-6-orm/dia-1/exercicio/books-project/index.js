01757fcf4259fdace1a317799e53b90bb326483f
Laís de Lima <laispsico92@gmail.com>
2023-10-26 17:22:06 -0300

    parte 5, 6, 7, 8

--- 03-Back-end/secao-6-orm/dia-1/exercicio/books-project/index.js
+++ 03-Back-end/secao-6-orm/dia-1/exercicio/books-project/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const BooksController = require ('./src/controllers/book.controller');
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
+
+app.get('/books', BooksController.getAll);
+app.get('/books/:id', BooksController.getById);
--- 03-Back-end/secao-6-orm/dia-1/exercicio/books-project/src/controllers/book.controller.js
+++ 03-Back-end/secao-6-orm/dia-1/exercicio/books-project/src/controllers/book.controller.js
@@ -1,6 +1,20 @@
-const bookService = require ('../services/book.service');
+const BookService = require ('../services/book.service');
 
 const getAll = async (req, res) => {
-  const books = await bookService.getAll();
+  const books = await BookService.getAll();
   return res.status(200).json(books);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const bookById = await BookService.getById(id);
+
+  if(!bookById) return res.status(404).json({ message: 'Book not found' });
+
+  res.status(200).json(bookById);
 }
+
+module.exports = {
+  getAll,
+  getById,
+};
