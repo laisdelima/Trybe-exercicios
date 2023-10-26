e7c41ec93cb2554eebaca9ec2bd5f12c3169508d
Laís de Lima <laispsico92@gmail.com>
2023-10-26 14:12:20 -0300

    cria nova migration e adiciona coluna 'phoneNum'

--- 03-Back-end/secao-6-orm/dia-1/app-with-sequelize/src/models/user.model.js
+++ 03-Back-end/secao-6-orm/dia-1/app-with-sequelize/src/models/user.model.js
@@ -2,7 +2,13 @@ const UserModel = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     fullName: DataTypes.STRING,
     email: DataTypes.STRING,
-  });
+    phoneNum: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    underscored: true,
+  }
+  );
 
   return User;
 };
