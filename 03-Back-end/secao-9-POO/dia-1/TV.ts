21bc0246287e8241ecd7e3946eb730d77bf6e5b3
Laís de Lima <laispsico92@gmail.com>
2024-01-23 14:05:51 -0300

    add encapsulamento ao código

--- 03-Back-end/secao-9-POO/dia-1/TV.ts
+++ 03-Back-end/secao-9-POO/dia-1/TV.ts
@@ -1,26 +1,38 @@
-// 1
 class Tv {
-  brand: string;
-  size: string;
-  resolution: string;
-  connections: string[];
-  connectedTo?: string;
+  private _brand: string;
+  private _size: string;
+  private _resolution: string;
+  private _connections: string[];
+  private _connectedTo?: string;
   
   constructor(b: string, s: string, r: string, con: string[]) {
 
-    this.brand = b;
-    this.size = s;
-    this.resolution = r;
-    this.connections = con;
+    this._brand = b;
+    this._size = s;
+    this._resolution = r;
+    this._connections = con;
   }
 
-  // 2
   turnOn(): void {
-    console.log(`A Tv ${this.brand} possui ${this.size}, ${this.resolution} de resolução,
-      com conexão ${this.connections}`);
+    console.log(`A Tv ${this._brand} possui ${this._size}, ${this._resolution} de resolução,
+      com conexão ${this._connections}`);
+  }
+
+  get connectedTo(): string | undefined {
+    return this._connectedTo;
+  }
+
+  set connectedTo(newValue: string | undefined) {
+    if (!newValue || this._connections.includes(newValue)) {
+      this._connectedTo = newValue;
+      console.log(this._connectedTo);
+    } else {
+      console.log('Sorry, connection unavailable!');
+    }
   }
 }
 
-// 3
 const tv1 = new Tv('LG', '70"', '4K', ['HDMI', 'Ethernet', 'Wi-Fi']);
 tv1.turnOn();
+tv1.connectedTo = 'HDMI';
+console.log('Connected to: ', tv1.connectedTo);
