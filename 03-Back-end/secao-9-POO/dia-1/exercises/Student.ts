69369a630bcc7887c722882ef3d3c9088d1c93e9
Laís de Lima <laispsico92@gmail.com>
2024-01-23 18:47:22 -0300

    exercise 2

--- 03-Back-end/secao-9-POO/dia-1/exercises/Student.ts
+++ 03-Back-end/secao-9-POO/dia-1/exercises/Student.ts
@@ -53,8 +53,29 @@ class Student {
     }
     this._projectGrade = newProjectGrade;
   }
+
+  // exercise 2
+  sumGrades(): number {
+    return [...this._examGrade, ...this._projectGrade]
+      .reduce((prevNote, note) => {
+        const nextNote = note + prevNote;
+
+        return nextNote;
+      }, 0);
+  }
+
+  sumAverageGrade(): number {
+    const sumGrades = this.sumGrades();
+    const totalSum = this._examGrade.length + this._projectGrade.length;
+
+    return Math.round(sumGrades / totalSum);
+  }
 }
 
 const personOne = new Student('202001011', 'Some student');
+personOne.setExamGrade([100, 95, 70, 80]);
+personOne.setProjectGrade([75, 90])
 
 console.log(personOne);
+console.log(personOne.sumGrades());
+console.log(personOne.sumAverageGrade());
